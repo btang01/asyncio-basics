25c9f07d8ff4e0e968fa2185d121352dd9a0aaa5
Brian Tang <briantang@Brians-MacBook-Pro-2.local>
2025-08-11 22:33:55 -0400

    added task group

--- basic-asyncio.py
+++ basic-asyncio.py
@@ -23,7 +23,6 @@ async def main_basic_nonconcurrent() -> object:
 
 # use tasks to run coroutines concurrently
 # receive results at the end all at the same time
-# errors not great, but optimized to run greedily switch tasks
 async def main_basic_concurrent() -> object:
     print("start of main basic concurrent coroutine")
 
@@ -38,6 +37,37 @@ async def main_basic_concurrent() -> object:
     print(result1, result2, result3)
     print("end of main basic concurrent coroutine")
 
+# use gather for a bunch of function calls, not great for errors
+async def main_gather_concurrent() -> object:
+    print("start of main gather concurrent coroutine")
+
+    results = await asyncio.gather(fetch_data_basic(1,2), fetch_data_basic(2,1), fetch_data_basic(3,3))
+
+    for result in results:
+        print(f"received result: {result}")
+
+    print("end of main gather concurrent coroutine")
+
+# use task group which is a newer feature
+async def main_task_group_concurrent() -> object:
+    print("start of main task group concurrent coroutine")
+    tasks = []
+
+    async with asyncio.TaskGroup() as tg:
+        for i, sleep_time in enumerate([2,1,3], start=1):
+            task = tg.create_task(fetch_data_basic(sleep_time, i))
+            tasks.append(task)
+    
+    # stop blocking
+    results = [task.result() for task in tasks]
+
+    for result in results:
+        print(f"received result: {result}")
+
+    print("end of main task group concurrent coroutine")
+
 if __name__=="__main__":
     asyncio.run(main_basic_nonconcurrent())
     asyncio.run(main_basic_concurrent())
+    asyncio.run(main_gather_concurrent())
+    asyncio.run(main_task_group_concurrent())
